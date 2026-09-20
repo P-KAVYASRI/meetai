6550cefebbe72a62c0c525d4d9c7f11492d1a0c8
P-KAVYASRI <kavyasri369369@gmail.com>
2026-09-20 12:46:26 +0530

    05:authentication socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,97 +1,18 @@
-"use client";
+import {headers} from "next/headers";
+import { redirect } from "next/navigation";
 
-import {useState} from "react";
+import {auth} from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
 
-import { authClient } from "@/lib/auth-client";
-import {Button} from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-
-
-
-export default function Home() {
-  const {data:session }=authClient.useSession();
-
-  const[name,setName]=useState("");
-  const[email,setEmail]=useState("");
-  const[password,setPassword]=useState("");
-
-  const onSubmit =  () => {
-  authClient.signUp.email({
-     email, // user email address
-     name,
-     password, // user password -> min 8 characters by default
-     
-    },{
-      onError:()=>{
-        window.alert("Something went wrong");
-      },
-      onSuccess:()=>{
-        window.alert("Success");
-      }
-    });
-}
-if(session){
-  return(
-   
-    <div className="flex flex-col p-4 gap-y-4">
-      <p> Logged in as {session.user.name}</p>
-      <Button onClick={()=> authClient.signOut()}>
-        Sign Out
-      </Button>
-    </div>
+const Page=async () =>{
+  const session =await auth.api.getSession({
+    headers: await headers(),
+  });
+  if(!session){
+    redirect("/sign-in");
+  }
   
-  );
-}
+  return <HomeView/>
 
-  return(
- <div className="flex flex-col gap-y-10">
-  <div className="p-4 flex flex-col gap-y-4">
-    <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)}/>
-    <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-    <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-    <Button onClick={onSubmit}>
-      Create User
-    </Button>
-  </div>
-
- {/* Login */}
-    <div className="p-4 flex flex-col gap-y-4">
-      <Input
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-
-      <Input
-        placeholder="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-
-      <Button
-        onClick={() => {
-          authClient.signIn.email(
-            {
-              email,
-              password,
-            },
-            {
-              onError: () => {
-                window.alert("Something went wrong");
-              },
-              onSuccess: () => {
-                window.alert("Login Success");
-              },
-            }
-          );
-        }}
-      >
-        Login
-      </Button>
-    </div>
-</div>
- 
-  
-  )
-}
+};
+export default Page;
--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import {useForm} from "react-hook-form";
+import { useRouter } from "next/navigation"; 
 import {zodResolver} from "@hookform/resolvers/zod";
 import {z} from "zod";
-
+import { useState } from "react";
 
 
 import { OctagonAlert, OctagonAlertIcon } from "lucide-react";
@@ -17,8 +18,8 @@ import {Alert, AlertTitle} from "@/components/ui/alert";
 import {Form, FormControl, FormField, FormItem,
      FormLabel, FormMessage} from "@/components/ui/form";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+
+import {FaGithub,FaGoogle } from "react-icons/fa";
 
 const formSchema=z.object({
     email: z.string().email(),
@@ -26,7 +27,7 @@ const formSchema=z.object({
 });
 
 export const SignInView = () => {
-    const router=useRouter();
+    const router = useRouter();
     const [error,setError]=useState<string | null>(null);
     const [pending,setPending]=useState(false);
     const form = useForm<z.infer<typeof formSchema>>({
@@ -43,21 +44,45 @@ export const SignInView = () => {
          authClient.signIn.email(
         {
             email: data.email, 
-            password: data.password
+            password: data.password, 
+            callbackURL: "/",
         },
         {
             onSuccess:()=>{
                 setPending(false);
                 router.push("/");
+                
             },
             onError:({error})=>{
                 setError(error.message);
             }
         }
     );
-    
-       
     };
+
+       const onSocial=(provider: "github" | "google") => {
+        setError(null);
+        setPending(true);
+
+         authClient.signIn.social(
+        {
+            provider: provider,
+            callbackURL: "/"
+        },
+        {
+            onSuccess:()=>{
+                setPending(false);
+                
+            },
+            onError:({error})=>{
+                setPending(false);
+                setError(error.message);
+            }
+        }
+    );
+};
+
+
 
   return (
     <div className="flex flex-col gap-6">
@@ -127,19 +152,21 @@ export const SignInView = () => {
            <div className="grid grid-cols-2 gap-4">
     <Button
         disabled={pending}
+        onClick ={()=>onSocial("google")}
         variant="outline" 
         type="button" 
         className="w-full"
         >
-            Google
+            <FaGoogle/>
     </Button>
     <Button
         disabled={pending}
+        onClick ={()=>onSocial("github")}
         variant="outline" 
         type="button" 
         className="w-full"
         >
-            GitHub
+          <FaGithub/>
     </Button>
                  </div>
                  <div className="text-center text-sm">
